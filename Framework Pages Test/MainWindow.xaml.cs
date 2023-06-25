596ecb28b6503d43cd2bdede085b0885d3e6c6eb
Björn Paulsen <bjorn.paulsen@decerno.se>
2023-06-25 16:05:06 +0200

    fix misspelled name

---
+++ Framework Pages Test/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace Framework_Pages_Test
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GoToPage2_Click(object sender, RoutedEventArgs e)
+        {
+            page1.Visibility = Visibility.Hidden;
+            page2.Visibility = Visibility.Visible;
+        }
+
+        private void GoToPage1_Click(object sender, RoutedEventArgs e)
+        {
+            page2.Visibility = Visibility.Hidden;
+            page1.Visibility = Visibility.Visible;
+        }
+    }
+}
